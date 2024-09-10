328f2392f66e16973ffd56a1a6a0110b99b63d1f
Kevin <kevin@patagoniachic.com>
2024-09-10 04:46:00 -0300

    updated api endpoint

--- api/routes/v1/location/index.ts
+++ api/routes/v1/location/index.ts
@@ -10,4 +10,8 @@ router.get("/location/:id", (req, res) => {
   return res.status(200).json({ id: req.params.id });
 });
 
+router.get("/location/all", (req, res) => {
+  return res.status(200).json(["location", "location2", "location3"]);
+});
+
 export default router;
