fc0e6cb0a8813c30be006d59f1d6e9e8bd6b93ef
Kevin <kevin@patagoniachic.com>
2024-09-10 04:20:21 -0300

    changed endpoint

--- src/routes/v1/location/index.ts
+++ src/routes/v1/location/index.ts
@@ -6,4 +6,8 @@ router.get("/location", (_, res) => {
   return res.status(200).json({ status: "ok" });
 });
 
+router.get("/location/:id", (req, res) => {
+  return res.status(200).json({ id: req.params.id });
+});
+
 export default router;
