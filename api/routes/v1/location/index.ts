8541cd6b72d85657c53b98e917110b088c12477a
Kevin <kevin@patagoniachic.com>
2024-09-10 04:47:46 -0300

    updated api endpoint

--- api/routes/v1/location/index.ts
+++ api/routes/v1/location/index.ts
@@ -5,13 +5,12 @@ const router = express.Router();
 router.get("/location", (_, res) => {
   return res.status(200).json({ status: "ok" });
 });
+router.get("/location/all", (_, res) => {
+  return res.status(200).json(["location", "location2", "location3"]);
+});
 
 router.get("/location/:id", (req, res) => {
   return res.status(200).json({ id: req.params.id });
-});
-
-router.get("/location/all", (req, res) => {
-  return res.status(200).json(["location", "location2", "location3"]);
 });
 
 export default router;
