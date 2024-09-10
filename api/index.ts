94b9b23481b0576ced7ea24c5757203c5783679f
Kevin <kevindana99@gmail.com>
2024-09-10 18:23:16 -0300

    server implement

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import Api from "./routes";
+import { PORT } from "./constants";
 
 const server = express();
 
 server.use(express.json());
 Api(server);
 
+server.listen(PORT,() => {
+    console.log('server on port' + PORT)
+})
 export default server;
