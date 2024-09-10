fe85297cd1466f26c8267c727d9ef2401348013c
Kevin <kevin@patagoniachic.com>
2024-09-09 22:47:04 -0300

    added nodemon

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,8 +5,8 @@ import { PORT } from "./constants";
 const server = express();
 
 server.use(express.json());
+Api(server);
 
 server.listen(PORT, () => {
   console.log(`server on port ${PORT}`);
 });
-Api(server);
